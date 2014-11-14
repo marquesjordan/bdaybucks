3b9636bf7785dad4d2b006debae1036ef98d1c19
Marques Jordan <marques.jordan81@gmail.com>
2014-11-14 12:36:19 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Birthdaybucks::Application.config.secret_key_base = '51716d6c8f792bd1630a881fad9e3f1baba14614037aaa575630d0046894acdfcf95b417dcf7b8b645dbbcf0365f38934529a3043eb48b51e15eb2edda32cb4d'
