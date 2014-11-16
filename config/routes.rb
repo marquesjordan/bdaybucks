9fea9ea63343c6b21b9c745f880d0f6dcb9ebd2c
thirdchance57 <thirdchance57@hotmail.com>
2014-11-15 19:09:46 -0800

    added post model and controller, put root on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Birthdaybucks::Application.routes.draw do
+
+  resources :post
+
+  root 'application#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
